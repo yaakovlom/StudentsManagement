0eecda254e2a1c0589d4a594c55f1b510e5636b4
Mordechai Greenwald <79059545+yaakovlom@users.noreply.github.com>
2022-04-04 14:07:42 +0300

    add csv file and TODO list. start a first architecture for the program

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+print_error_and_exit(const char* error, int error_code);
 
 int main()
 {
+	FILE* in_file;
+	char ch = '\n';
+
+	if (!(in_file = fopen("DB_studs.csv", "r"))) print_error_and_exit("Cannot open input file", -1);
+
+	while (!feof(in_file) && ch)
+	{
+		printf("%c", ch);
+		fscanf(in_file, "%c", &ch);
+	}
+
+	fclose(in_file);
 	return 0;
+}
+
+print_error_and_exit(const char* error, int error_code)
+{
+	printf("%s", error);
+	exit(error_code);
 }
